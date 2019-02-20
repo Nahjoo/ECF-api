c163b304907fa281ecd28031c67009245fb596b9
DEVnahjo <nahjo@nahjo.home>
2019-02-20 19:41:03 +0100

    add readme technical

--- tests/TestFonctionalTest.php
+++ tests/TestFonctionalTest.php
@@ -9,9 +9,10 @@ class TestFonctionalTest extends WebTestCase
     public function testSomething()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/login');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Hello World', $crawler->filter('h1')->text());
+        $this->assertContains('Username', $crawler->filter('body')->text());
+        $this->assertContains('Password', $crawler->filter('body')->text());
     }
 }
